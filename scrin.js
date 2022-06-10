a1b0d1083963d7416cdb400325dcc9115434c3e0
мир <106350647+mir3133031@users.noreply.github.com>
2022-06-10 17:00:22 +0500

    Add files via upload

---
+++ scrin.js
@@ -0,0 +1,15 @@
+let arr = ['Aleksey', 'Kartoshka', 'Margarita' , 'Morgenshtern', 'Monica']
+let index = prompt(`Что не нравится ?`)
+if(arr.splice(index, 1 ) >= `5`){
+alert(arr)
+
+}else  {
+
+    
+alert(
+    `go ot suda`
+) 
+    
+   
+}
+
